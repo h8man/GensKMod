cca911137fa50b0b2f8a803996e0ef2966661af9
KanedaFr <bitbucket@spritesmind.net>
2013-10-11 15:08:01 +0200

    Full Visual Studio Solution

--- src/comp_m68k.bat
+++
@@ -1,8 +0,0 @@
-rem Main 68000 compilation (Main68k\star.c has been compiled before)
-
-@cd starscream\main68k\release
-star temp.asm -hog -name main68k_
-..\..\..\..\nasmw -f win32 temp.asm -o ..\..\..\Gens\release\main68k.obj
-@cd..\..\..
-
-pause
--- src/comp_s68k.bat
+++
@@ -1,7 +0,0 @@
-rem Main 68000 compilation (Sub68k\star.c has been compiled before)
-
-@cd starscream\sub68k\release
-star temp.asm -hog -name sub68k_
-..\..\..\..\nasmw -f win32 temp.asm -o ..\..\..\Gens\release\sub68k.obj
-@cd..\..\..
-pause
