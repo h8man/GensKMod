599fa326f319c52f90f20b0ef7190a2f00207cb6
h8man <vmazurenko@bk.ru>
2021-04-25 01:42:47 +0300

    Added sprite copy to clipboard

--- src/Gens/kmod/utils.h
+++ src/Gens/kmod/utils.h
@@ -11,6 +11,8 @@ void Ansiview(unsigned char *addr, unsigned char *dest);
 
 void drawTile(HDC hDCMain, unsigned short int numTile, WORD x, WORD y, UCHAR pal, UCHAR zoom);
 
+int CopyToClipboard(int Type, unsigned char* Buffer, size_t buflen, BOOL clear); // feos added this
+
 #ifdef __cplusplus
 };
 #endif
